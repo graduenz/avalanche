cb55bb5dc672c78bbb515b8ee896f4284ec14c68
Guilherme Augusto Raduenz <graduenz@gmail.com>
2022-12-17 10:46:33 -0300

    feat: added CsvHelper parsing implementation

---
+++ src/implementations/Avalanche.CsvHelper/CsvHelperParsingOperation.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using CsvHelper;
+
+namespace Avalanche.CsvHelper;
+
+public class CsvHelperParsingOperation<TModel> : IPipelineOperation
+    where TModel : IStreamingItem
+{
+    private readonly Func<StreamReader, CsvReader> _csvReaderFactory;
+
+    public CsvHelperParsingOperation() : this(reader => new CsvReader(reader, CultureInfo.InvariantCulture))
+    {
+    }
+
+    public CsvHelperParsingOperation(Func<StreamReader, CsvReader> csvReaderFactory)
+    {
+        _csvReaderFactory = csvReaderFactory;
+    }
+
+    public async Task RunAsync(PipelineContext context, PipelineOperationDelegate next)
+    {
+        context.BaseStream.Seek(0, SeekOrigin.Begin);
+        
+        using var reader = new StreamReader(context.BaseStream, leaveOpen: true);
+        using var csv = _csvReaderFactory(reader);
+        var records = csv.GetRecords<TModel>();
+
+        context.Items.Clear();
+        foreach (var record in records)
+            context.Items.Add(record);
+
+        await next(context);
+    }
+}
