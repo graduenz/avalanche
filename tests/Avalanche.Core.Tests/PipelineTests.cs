a10836954e86b63208da7a90fcecba063f629f7c
Guilherme Augusto Raduenz <graduenz@gmail.com>
2022-12-17 14:09:48 -0300

    test: created basic tests for Pipeline

---
+++ tests/Avalanche.Core.Tests/PipelineTests.cs
@@ -0,0 +1,55 @@
+﻿using Avalanche.Tests.Shared;
+using FluentAssertions;
+
+namespace Avalanche.Core.Tests;
+
+public class PipelineTests : AvalancheTestCase
+{
+    [Fact]
+    public async Task ExecuteAsync_WhenUniqueIdIsEmpty_Throws()
+    {
+        // Arrange
+        var pipeline = PipelineFactory.SetupPipeline().Build();
+
+        // Act
+        var act = async () =>
+            await pipeline.ExecuteAsync(Guid.Empty, "test.csv", TestUtils.GetSharedEmbeddedResource(Employees50));
+
+        // Assert
+        await act.Should().ThrowAsync<ArgumentException>()
+            .WithParameterName("uniqueId")
+            .WithMessage("Parameter uniqueId can't be an empty Guid*");
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    public async Task ExecuteAsync_WhenNameIsEmpty_Throws(string name)
+    {
+        // Arrange
+        var pipeline = PipelineFactory.SetupPipeline().Build();
+
+        // Act
+        var act = async () =>
+            await pipeline.ExecuteAsync(Guid.NewGuid(), name, TestUtils.GetSharedEmbeddedResource(Employees50));
+
+        // Assert
+        await act.Should().ThrowAsync<ArgumentNullException>()
+            .WithParameterName("name");
+    }
+
+    [Fact]
+    public async Task ExecuteAsync_WhenBaseStreamIsNull_Throws()
+    {
+        // Arrange
+        var pipeline = PipelineFactory.SetupPipeline().Build();
+
+        // Act
+        var act = async () =>
+            await pipeline.ExecuteAsync(Guid.NewGuid(), "test.csv", null!);
+
+        // Assert
+        await act.Should().ThrowAsync<ArgumentNullException>()
+            .WithParameterName("baseStream");
+    }
+}
